d5776691e40512f44568ea5b6ffb94f5dd50b6ab
JaeWooYi <wodn0101@gmail.com>
2022-03-30 16:48:31 +0900

    4st

--- MVC01/src/kr/bit/controller/CalcController.java
+++ MVC01/src/kr/bit/controller/CalcController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.bit.model.MyCalc;
+
 @WebServlet("/calc.do")
 public class CalcController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -17,15 +19,26 @@ public class CalcController extends HttpServlet {
 		int su2 = Integer.parseInt(request.getParameter("su2"));
 		
 		// 2. su1과 su2의 합을 구해보자.
+		// 이부분(비지니스로직)을 모델로 빼보자
 //		int sum = 0;	// 초기화
-//		for(int i = su1; i < su2; i++) {
+//		for(int i = su1; i <= su2; i++) {
 //			sum += i;
 //		}
 	
-		int sum = su1 + su2;
+		//int sum = su1 + su2;
 		
+		MyCalc myCalc = new MyCalc();
+		int sum = myCalc.hap(su1, su2);
+		
+		// 응답하는 부분(프리젠테이션로직 = View = JSP)
 		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>");
 		out.println("total = " + sum);
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
