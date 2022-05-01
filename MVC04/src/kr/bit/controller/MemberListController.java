bd5ecd4d17ee2d0356cc9fb044c6ca54cedb97f1
JaeWooYi <wodn0101@gmail.com>
2022-05-01 21:14:02 +0900

    Misery

--- MVC04/src/kr/bit/controller/MemberListController.java
+++ MVC04/src/kr/bit/controller/MemberListController.java
@@ -1,5 +1,32 @@
 package kr.bit.controller;
 
-public class MemberListController {
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.bit.model.MemberDAO;
+import kr.bit.model.MemberVO;
+
+public class MemberListController implements Controller{
+
+	@Override
+	// public String requestHandler -> String : 다음페이지의 정보를 리턴한다
+	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// POJO가 해야 할 일에 대한 범
+		// 1. MODEL연동
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.memberList();
+		// 최종적으로는 list를 member/memberList.jsp에다가 줘야한
+		// 2. 객체 바인딩
+		request.setAttribute("list", list);
+		
+		// 3. View page의 정보
+		// 다음페이지는 
+		return "member/memberList.jsp";	// requestHandler의 type이 void가 아니라 String으로 가야겠지?
+	}
 
 }
