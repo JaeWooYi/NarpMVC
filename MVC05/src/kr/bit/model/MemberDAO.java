748b81cabe0115c214a2aa954f3f468f1445bd87
JaeWooYi <wodn0101@gmail.com>
2022-05-10 20:53:06 +0900

    TuesDay

--- MVC05/src/kr/bit/model/MemberDAO.java
+++ MVC05/src/kr/bit/model/MemberDAO.java
@@ -3,7 +3,10 @@ package kr.bit.model;
 // JDBC -> myBatis, JPA
 
 import java.io.InputStream;
+import java.util.List;
+
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -19,5 +22,23 @@ public class MemberDAO {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// 회원전체 리스트 보기
+	public List<MemberVO> memberList() {
+		// [Connection + Statement] -> sqlSession
+		SqlSession session = sqlSessionFactory.openSession();
+		List<MemberVO> list = session.selectList("memberList");
+		session.close();
+		return list;
+	}
+	
+	// 회원가입 기능
+	public int memberInsert(MemberVO vo){
+		SqlSession session = sqlSessionFactory.openSession();
+		int cnt = session.insert("memberInsert", vo);
+		session.commit();
+		session.close();
+		return cnt;
 	}
 }
